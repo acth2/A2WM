429d334f2345d7ba0e1f17c3af9890876ced0b5d
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-19 12:11:32 +0200

    Update and rename MinimizedPosInt.cpp to MinimizedPosArray.cpp

---
+++ qt/windowmanager/win/MinimizedPosArray.cpp
@@ -0,0 +1,41 @@
+#include "MinimizedPosArray.h"
+
+MinimizedPosArray::MinimizedPosArray() {
+    for (int i = 0; i < 10; ++i) {
+        minimizedSlots.push_back(i * 150);
+    }
+}
+
+int MinimizedPosArray::getSmallestAvailable() {
+    std::lock_guard<std::mutex> lock(mtx);
+
+    for (int pos : minimizedSlots) {
+        if (pos != -1) {
+            return pos;
+        }
+    }
+    throw std::runtime_error("No available positions.");
+}
+
+void MinimizedPosArray::markPositionAsTaken(int pos) {
+    std::lock_guard<std::mutex> lock(mtx);
+
+    auto it = std::find(minimizedSlots.begin(), minimizedSlots.end(), pos);
+    if (it != minimizedSlots.end()) {
+        *it = -1;
+    } else {
+        throw std::invalid_argument("Position not found.");
+    }
+}
+
+void MinimizedPosArray::freePosition(int pos) {
+    std::lock_guard<std::mutex> lock(mtx);
+
+    for (int i = 0; i < minimizedSlots.size(); ++i) {
+        if (minimizedSlots[i] == -1 && i * 150 == pos) {
+            minimizedSlots[i] = pos;
+            return;
+        }
+    }
+    throw std::invalid_argument("Position not found to free.");
+}

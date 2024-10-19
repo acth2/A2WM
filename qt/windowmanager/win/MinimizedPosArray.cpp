334731966af534457f1909ce9ca9c50d0afb9215
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-19 23:24:49 +0200

    Update MinimizedPosArray.cpp

--- qt/windowmanager/win/MinimizedPosArray.cpp
+++ qt/windowmanager/win/MinimizedPosArray.cpp
@@ -2,7 +2,7 @@
 
 MinimizedPosArray::MinimizedPosArray() {
     for (int i = 0; i < 50; ++i) {
-        minimizedSlots.push_back(i * 100);
+        minimizedSlots.push_back(i * 96);
     }
 }
 
@@ -32,7 +32,7 @@ void MinimizedPosArray::freePosition(int pos) {
     std::lock_guard<std::mutex> lock(mtx);
 
     for (int i = 0; i < minimizedSlots.size(); ++i) {
-        if (minimizedSlots[i] == -1 && i * 100 == pos) {
+        if (minimizedSlots[i] == -1 && i * 96 == pos) {
             minimizedSlots[i] = pos;
             return;
         }

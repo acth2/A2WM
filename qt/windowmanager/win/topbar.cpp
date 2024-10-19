60f7c2674fddfd39164b6043b027d7d6e2e3a8a7
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-20 01:29:27 +0200

    Update topbar.cpp

--- qt/windowmanager/win/topbar.cpp
+++ qt/windowmanager/win/topbar.cpp
@@ -317,7 +317,7 @@ void TopBar::mousePressEvent(QMouseEvent *event) {
     if (isMinimized) {
         QPoint globalPos = QCursor::pos();
         trackedWindow->setGeometry(originalGeometry);
-        trackedWindow->setPosition(globalPos.x(), globalPos.y() - 38);
+        trackedWindow->setPosition(globalPos.x(), globalPos.y() + 50);
         trackedWindow->show();
 
         this->resize(originalTopBarWidth, this->height());

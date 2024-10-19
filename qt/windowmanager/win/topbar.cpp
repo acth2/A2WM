c87e0b725a95eee2be20203668e26dfaf30ee29a
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-20 01:44:28 +0200

    Update topbar.cpp

--- qt/windowmanager/win/topbar.cpp
+++ qt/windowmanager/win/topbar.cpp
@@ -210,11 +210,11 @@ void TopBar::minimizeWindow() {
         QScreen *screen = QApplication::primaryScreen();
         QRect screenGeometry = screen->geometry();
 
-        QString origTitle = titleLabel.text();
+        QString origTitle = titleLabel->text();
 
         if (origTitle.size() > 13) {
             QString newTitle = origTitle.left(13) + "-";
-            titleLabel.setText(newTitle);
+            titleLabel->setText(newTitle);
         }
 
         this->resize(25, this->height());

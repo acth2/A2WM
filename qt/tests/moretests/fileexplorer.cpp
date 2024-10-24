a56ab9eb513c1dcd0e24aea334c75fd7b6adbd3d
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-24 15:35:41 +0200

    Update fileexplorer.cpp

--- qt/tests/moretests/fileexplorer.cpp
+++ qt/tests/moretests/fileexplorer.cpp
@@ -6,6 +6,11 @@
 #include <QListView>
 #include <QVBoxLayout>
 #include <QHeaderView>
+#include <QPushButton>
+#include <QMenu>
+#include <QInputDialog>
+#include <QMessageBox>
+#include <QDir>
 
 class FileExplorer : public QMainWindow {
     Q_OBJECT
@@ -36,12 +41,62 @@ public:
                     listView->setRootIndex(fileModel->setRootPath(path));
                 });
 
+        QPushButton* homeButton = new QPushButton("Home", this);
+        connect(homeButton, &QPushButton::clicked, [=]() {
+            QString homePath = QDir::homePath();
+            listView->setRootIndex(fileModel->setRootPath(homePath));
+            treeView->setRootIndex(dirModel->index(homePath));
+        });
+
         QSplitter* splitter = new QSplitter(this);
         splitter->addWidget(treeView);
         splitter->addWidget(listView);
         splitter->setStretchFactor(1, 1);
 
-        setCentralWidget(splitter);
+        QVBoxLayout* layout = new QVBoxLayout();
+        layout->addWidget(homeButton);
+        layout->addWidget(splitter);
+        QWidget* centralWidget = new QWidget(this);
+        centralWidget->setLayout(layout);
+        setCentralWidget(centralWidget);
+
+        listView->setContextMenuPolicy(Qt::CustomContextMenu);
+        connect(listView, &QListView::customContextMenuRequested, [=](const QPoint& pos) {
+            QModelIndex index = listView->indexAt(pos);
+            if (index.isValid()) {
+                QMenu menu(this);
+                QAction* deleteAction = menu.addAction("Delete");
+                QAction* createFileAction = menu.addAction("Create New File");
+
+                connect(deleteAction, &QAction::triggered, [=]() {
+                    QString filePath = fileModel->fileInfo(index).absoluteFilePath();
+                    if (QFile::remove(filePath)) {
+                        QMessageBox::information(this, "Success", "File deleted.");
+                    } else {
+                        QMessageBox::warning(this, "Error", "Failed to delete file.");
+                    }
+                });
+
+                connect(createFileAction, &QAction::triggered, [=]() {
+                    bool ok;
+                    QString fileName = QInputDialog::getText(this, "Create New File", "File name:", QLineEdit::Normal, "", &ok);
+                    if (ok && !fileName.isEmpty()) {
+                        QString dirPath = fileModel->fileInfo(index).absolutePath();
+                        QString newFilePath = QDir(dirPath).filePath(fileName);
+                        QFile newFile(newFilePath);
+                        if (newFile.open(QIODevice::ReadWrite)) {
+                            newFile.close();
+                            QMessageBox::information(this, "Success", "File created.");
+                        } else {
+                            QMessageBox::warning(this, "Error", "Failed to create file.");
+                        }
+                    }
+                });
+
+                menu.exec(QCursor::pos());
+            }
+        });
+
         setWindowTitle("Qt File Explorer");
         resize(800, 600);
     }

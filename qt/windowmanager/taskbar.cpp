6175097ff43a6a8836b142b890a5e6674d6546bd
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-25 16:54:13 +0200

    Update taskbar.cpp

--- qt/windowmanager/taskbar.cpp
+++ qt/windowmanager/taskbar.cpp
@@ -44,6 +44,7 @@ TaskBar::TaskBar(QWidget *parent) : QWidget(parent) {
     setLayout(layout);
 
     QFont font("Segoe UI Semibold");
+    font.setBold(true);
     font.setPixelSize(15);
 
     popup = new QLabel(nullptr);
@@ -70,6 +71,7 @@ TaskBar::TaskBar(QWidget *parent) : QWidget(parent) {
         popup->setStyleSheet("background-color: #fff; border: 1px solid #000000;"); 
         popupExtension->setStyleSheet("background-color: #fff; border: 1px solid #000000;"); 
     }
+    username->setStyleSheet("background: transparent;");
     
     popup->hide();
     popupExtension->hide();
@@ -132,7 +134,7 @@ void TaskBar::showPopup() {
     } else {
         popup->move(0, height() * 5.7);
         userLogo->move(175, popup->y() * 0.75);
-        username->move(userLogo->x() - username->width() - 5, userLogo->y());
+        username->move(userLogo->x(), userLogo->y() + userLogo->height() + 5);
         popupExtension->move(435, 275);
         popup->show();
         popup->setWindowFlags(windowFlags());

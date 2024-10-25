18452aa31868e0dacf03f2c70d40f8ea78bfb11a
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-26 00:16:22 +0200

    Update ClickableLabel.h

--- qt/windowmanager/utils/ClickableLabel.h
+++ qt/windowmanager/utils/ClickableLabel.h
@@ -6,15 +6,20 @@ class ClickableLabel : public QLabel {
     Q_OBJECT
 
 public:
-    explicit ClickableLabel(const QString &text, QWidget *parent = nullptr) : QLabel(text, parent) {}
+    explicit ClickableLabel(const QString &text, const QString &dirPath, QWidget *parent = nullptr)
+        : QLabel(text, parent), directoryPath(dirPath) {}
+
+    QString getDirectoryPath() const { return directoryPath; }
 
 signals:
-    void clicked(const QString &labelText);
+    void clicked(const QString &directoryPath);
 
 protected:
     void mousePressEvent(QMouseEvent *event) override {
-        if (event->button() == Qt::LeftButton) {
-            emit clicked(this->text());
-        }
+        emit clicked(directoryPath);
+        QLabel::mousePressEvent(event);
     }
+
+private:
+    QString directoryPath;
 };

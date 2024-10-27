4be24d73f1d4addde386143e007d2b9a2e6cf157
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-27 10:01:08 +0100

    Update ClickableLabel.h

--- qt/windowmanager/utils/ClickableLabel.h
+++ qt/windowmanager/utils/ClickableLabel.h
@@ -1,13 +1,21 @@
 #include <QLabel>
 #include <QWidget>
 #include <QMouseEvent>
+#include <QPalette>
+#include <QGraphicsOpacityEffect>
 
 class ClickableLabel : public QLabel {
     Q_OBJECT
 
 public:
     explicit ClickableLabel(const QString &text, const QString &dirPath, QWidget *parent = nullptr)
-        : QLabel(text, parent), directoryPath(dirPath) {}
+        : QLabel(text, parent), directoryPath(dirPath) {
+        opacityEffect = new QGraphicsOpacityEffect(this);
+        setGraphicsEffect(opacityEffect);
+        opacityEffect->setOpacity(1.0);
+
+        setAttribute(Qt::WA_TranslucentBackground);
+    }
 
     QString getDirectoryPath() const { return directoryPath; }
 
@@ -16,10 +24,25 @@ signals:
 
 protected:
     void mousePressEvent(QMouseEvent *event) override {
+        QPalette palette = this->palette();
+        palette.setColor(QPalette::Window, Qt::black);
+        setPalette(palette);
+        update();
+
         emit clicked(directoryPath);
         QLabel::mousePressEvent(event);
     }
 
+    void mouseReleaseEvent(QMouseEvent *event) override {
+        QPalette palette = this->palette();
+        palette.setColor(QPalette::Window, Qt::transparent);
+        setPalette(palette);
+        update();
+        
+        QLabel::mouseReleaseEvent(event);
+    }
+
 private:
     QString directoryPath;
+    QGraphicsOpacityEffect *opacityEffect;
 };

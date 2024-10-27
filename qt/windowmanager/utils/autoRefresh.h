2da7f75c38b36478a55c26e106727462a83dc421
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-27 17:47:11 +0100

    Create autoRefresh.h

---
+++ qt/windowmanager/utils/autoRefresh.h
@@ -0,0 +1,33 @@
+#include <QApplication>
+#include <QTimer>
+#include <QProcess>
+#include <QScreen>
+#include <QDebug>
+
+class DisplayRefresher : public QObject {
+    Q_OBJECT
+
+public:
+    DisplayRefresher(QObject *parent = nullptr) : QObject(parent) {
+        QTimer *timer = new QTimer(this);
+        connect(timer, &QTimer::timeout, this, &DisplayRefresher::refreshDisplay);
+        timer->start(100);
+    }
+
+private slots:
+    void refreshDisplay() {
+        QString display = getCurrentDisplay();
+
+        if (!display.isEmpty()) {
+            QProcess::execute("xrefresh", QStringList() << "-display" << display);
+        } else {
+            qWarning() << "Unable to determine the current display number.";
+        }
+    }
+
+private:
+    QString getCurrentDisplay() {
+        QByteArray displayEnv = qgetenv("DISPLAY");
+        return !displayEnv.isEmpty() ? QString(displayEnv) : QString();
+    }
+};

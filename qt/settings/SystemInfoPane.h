38de57064ba454c98f4d0207bb8acbf3cb8eaa14
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-28 12:50:57 +0100

    Update SystemInfoPane.h

--- qt/settings/SystemInfoPane.h
+++ qt/settings/SystemInfoPane.h
@@ -14,29 +14,24 @@ public:
         QVBoxLayout *layout = new QVBoxLayout(this);
 
         QString wmVersion = readFileContents("/usr/cydra/info/version").trimmed();
-        QLabel *wmLabel = new QLabel("Window Manager: A2WM (AcTh2WindowManager) - Version: " + wmVersion);
-        layout->addWidget(wmLabel);
+        addStyledLabel(layout, "Window Manager:", "A2WM (AcTh2WindowManager) - Version: " + wmVersion);
 
-        QLabel *bootLabel = new QLabel("BIOS Boot-Time: " + getBootTime());
-        layout->addWidget(bootLabel);
+        QString bootTime = getBootTime();
+        addStyledLabel(layout, "BIOS Boot-Time:", bootTime);
 
-        QLabel *ramLabel = new QLabel("Total RAM: " + getTotalRam() + " MB");
-        layout->addWidget(ramLabel);
+        QString ramSize = getTotalRAM();
+        addStyledLabel(layout, "Total RAM:", ramSize + " MB");
 
         QStorageInfo storage = QStorageInfo::root();
-        QLabel *diskLabel = new QLabel("Disk Space Left: " + QString::number(storage.bytesAvailable() / (1024 * 1024 * 1024)) + " GB");
-        layout->addWidget(diskLabel);
+        addStyledLabel(layout, "Disk Space Left:", QString::number(storage.bytesAvailable() / (1024 * 1024 * 1024)) + " GB");
 
-        QLabel *bitsLabel = new QLabel("System Architecture: " + QString::number(QSysInfo::WordSize) + "-bit");
-        layout->addWidget(bitsLabel);
+        addStyledLabel(layout, "System Architecture:", QString::number(QSysInfo::WordSize) + "-bit");
+        addStyledLabel(layout, "Processor:", getCpuInfo());
 
-        QLabel *cpuLabel = new QLabel("Processor: " + getCpuInfo());
-        layout->addWidget(cpuLabel);
-
-        QLabel *userLabel = new QLabel("Username: " + qgetenv("USER"));
-        QLabel *systemLabel = new QLabel("System Name: " + QSysInfo::machineHostName());
-        layout->addWidget(userLabel);
-        layout->addWidget(systemLabel);
+        QString username = qgetenv("USER");
+        QString systemName = QSysInfo::machineHostName();
+        addStyledLabel(layout, "Username:", username);
+        addStyledLabel(layout, "System Name:", systemName);
     }
 
 private:
@@ -53,21 +48,14 @@ private:
         proc.start("systemd-analyze", QStringList() << "time");
         proc.waitForFinished();
         QString output = proc.readAllStandardOutput();
-        return output.isEmpty() ? "Unavailable" : output;
+        return output.isEmpty() ? "Root permissions required" : output;
     }
 
-    QString getTotalRam() {
-        QFile file("/proc/meminfo");
-        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-            while (!file.atEnd()) {
-                QString line = file.readLine();
-                if (line.startsWith("MemTotal:")) {
-                    QStringList parts = line.split(QRegExp("\\s+"), Qt::SkipEmptyParts);
-                    if (parts.size() >= 2) {
-                        return QString::number(parts[1].toInt() / 1024);
-                    }
-                }
-            }
+    QString getTotalRAM() {
+        QFile memInfo("/proc/meminfo");
+        if (memInfo.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QString content = memInfo.readLine();
+            return content.section(" ", -2, -2).trimmed();
         }
         return "Unavailable";
     }
@@ -78,5 +66,11 @@ private:
         proc.waitForFinished();
         QString output = proc.readAllStandardOutput();
         return output.section("Model name:", 1, 1).trimmed();
+    }
+
+    void addStyledLabel(QVBoxLayout *layout, const QString &labelText, const QString &valueText) {
+        QLabel *label = new QLabel("<b>" + labelText + "</b> " + valueText, this);
+        label->setStyleSheet("font-size: 16px; margin-bottom: 6px;");
+        layout->addWidget(label);
     }
 };

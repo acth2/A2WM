658077cf0246eccdbfbdf5d2058b2a3756f3285d
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-28 12:57:14 +0100

    Update SystemInfoPane.h

--- qt/settings/SystemInfoPane.h
+++ qt/settings/SystemInfoPane.h
@@ -12,29 +12,41 @@ class SystemInfoPane : public QWidget {
 public:
     SystemInfoPane(QWidget *parent = nullptr) : QWidget(parent) {
         QVBoxLayout *layout = new QVBoxLayout(this);
+        layout->setSpacing(2);
 
         QString wmVersion = readFileContents("/usr/cydra/info/version").trimmed();
-        addStyledLabel(layout, "Window Manager:", "A2WM (AcTh2WindowManager) - Version: " + wmVersion);
+        QLabel *wmLabel = createBoldLabel("Window Manager: A2WM (AcTh2WindowManager) - Version: " + wmVersion);
+        layout->addWidget(wmLabel);
 
-        QString bootTime = getBootTime();
-        addStyledLabel(layout, "BIOS Boot-Time:", bootTime);
+        QLabel *bootLabel = createBoldLabel("BIOS Boot-Time: " + getBootTime());
+        layout->addWidget(bootLabel);
 
-        QString ramSize = getTotalRAM();
-        addStyledLabel(layout, "Total RAM:", ramSize + " MB");
+        QLabel *ramLabel = createBoldLabel("Total RAM: " + getTotalRam() + " MB");
+        layout->addWidget(ramLabel);
 
         QStorageInfo storage = QStorageInfo::root();
-        addStyledLabel(layout, "Disk Space Left:", QString::number(storage.bytesAvailable() / (1024 * 1024 * 1024)) + " GB");
+        QLabel *diskLabel = createBoldLabel("Disk Space Left: " + QString::number(storage.bytesAvailable() / (1024 * 1024 * 1024)) + " GB");
+        layout->addWidget(diskLabel);
 
-        addStyledLabel(layout, "System Architecture:", QString::number(QSysInfo::WordSize) + "-bit");
-        addStyledLabel(layout, "Processor:", getCpuInfo());
+        QLabel *bitsLabel = createBoldLabel("System Architecture: " + QString::number(QSysInfo::WordSize) + "-bit");
+        layout->addWidget(bitsLabel);
 
-        QString username = qgetenv("USER");
-        QString systemName = QSysInfo::machineHostName();
-        addStyledLabel(layout, "Username:", username);
-        addStyledLabel(layout, "System Name:", systemName);
+        QLabel *cpuLabel = createBoldLabel("Processor: " + getCpuInfo());
+        layout->addWidget(cpuLabel);
+
+        QLabel *userLabel = createBoldLabel("Username: " + qgetenv("USER"));
+        QLabel *systemLabel = createBoldLabel("System Name: " + QSysInfo::machineHostName());
+        layout->addWidget(userLabel);
+        layout->addWidget(systemLabel);
     }
 
 private:
+    QLabel* createBoldLabel(const QString &text) {
+        QLabel *label = new QLabel(text);
+        label->setStyleSheet("font-weight: bold; font-size: 14px; margin-bottom: 2px;");
+        return label;
+    }
+
     QString readFileContents(const QString &filePath) {
         QFile file(filePath);
         if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
@@ -48,14 +60,15 @@ private:
         proc.start("systemd-analyze", QStringList() << "time");
         proc.waitForFinished();
         QString output = proc.readAllStandardOutput();
-        return output.isEmpty() ? "Root permissions required" : output;
+        return output.isEmpty() ? "Unavailable (Requires root)" : output;
     }
 
-    QString getTotalRAM() {
+    QString getTotalRam() {
         QFile memInfo("/proc/meminfo");
         if (memInfo.open(QIODevice::ReadOnly | QIODevice::Text)) {
-            QString content = memInfo.readLine();
-            return content.section(" ", -2, -2).trimmed();
+            QString line = memInfo.readLine();
+            memInfo.close();
+            return line.split(":").last().trimmed().split(" ").first();
         }
         return "Unavailable";
     }
@@ -65,12 +78,8 @@ private:
         proc.start("lscpu");
         proc.waitForFinished();
         QString output = proc.readAllStandardOutput();
-        return output.section("Model name:", 1, 1).trimmed();
-    }
-
-    void addStyledLabel(QVBoxLayout *layout, const QString &labelText, const QString &valueText) {
-        QLabel *label = new QLabel("<b>" + labelText + "</b> " + valueText, this);
-        label->setStyleSheet("font-size: 16px; margin-bottom: 6px;");
-        layout->addWidget(label);
+        QString cpuName = output.section("Model name:", 1, 1).simplified();
+        QString cpuGHz = output.section("CPU MHz:", 1, 1).simplified();
+        return cpuName + " " + QString::number(cpuGHz.toDouble() / 1000, 'f', 2) + " GHz";
     }
 };

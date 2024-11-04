556d9155e16d1fbed680de3a45600ec55def0651
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-11-04 13:11:25 +0100

    Update windowmanager.cpp

--- qt/windowmanager/windowmanager.cpp
+++ qt/windowmanager/windowmanager.cpp
@@ -143,7 +143,7 @@ void WindowManager::listExistingWindows() {
 
                 existingWindows.insert(name.toUpper(), QSize(0, 0));
 
-                if (trackedWindows.contains(name)) {
+                if (existingWindows.contains(name)) {
                     createAndTrackWindow(child, name, attributes.width, attributes.height);
                     continue;
                 }

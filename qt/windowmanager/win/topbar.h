99411041a1235bcd701c0293f34a270f06b85278
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-11-07 20:10:44 +0100

    Update topbar.h

--- qt/windowmanager/win/topbar.h
+++ qt/windowmanager/win/topbar.h
@@ -22,7 +22,7 @@ class TopBar : public QWidget {
     Q_OBJECT
 
 public:
-    explicit TopBar(QWindow *parentWindow, WindowManager *manager, bool backupWindow, QWidget *parent = nullptr);
+    explicit TopBar(QWindow *parentWindow, WindowManager *manager, QWidget *parent = nullptr);
     void updatePosition();
     void setTitle(const QString &title);
     QWindow* getTrackedWindow() const;

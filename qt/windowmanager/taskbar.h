9f5bb9fc43f5c53e2d4789515ac50f7908b4ffc3
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-11-10 01:04:36 +0100

    Update taskbar.h

--- qt/windowmanager/taskbar.h
+++ qt/windowmanager/taskbar.h
@@ -50,6 +50,9 @@ private:
 
     void onLabelClicked(const QString &labelText);
     void onLabelClickedExec(const QString &execCommand);
+
+    void addWindowToTaskbar(QWindow *window);
+    void updateTaskbarItems();
 };
 
 #endif // TASKBAR_H

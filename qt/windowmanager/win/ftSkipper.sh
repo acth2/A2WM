2765e5b2e544400f521cf59a3f03207c1cd3cb28
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-11-17 11:52:39 +0100

    Update ftSkipper.sh

--- qt/windowmanager/win/ftSkipper.sh
+++ qt/windowmanager/win/ftSkipper.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 refresh_rate=$(xrandr | grep '*' | awk '{print $2}' | tr -d '+*')
+
 if [[ -z "$refresh_rate" || "$refresh_rate" -le 0 ]]; then
-    echo "Failed to get refresh rate. Using default 60 Hz."
+    echo "Failed to detect refresh rate. Using default 60 Hz."
     refresh_rate=60
 fi
 
-delay=$(awk "BEGIN {print 1/$refresh_rate}")
-echo "Running at ${refresh_rate} Hz with a delay of ${delay} seconds."
+delay=$(awk "BEGIN {print 1000000 / $refresh_rate}")
+
+echo "Detected refresh rate: ${refresh_rate} Hz"
+echo "Adjusting loop to run every ${delay} microseconds."
 
 # Main loop
 while true; do
     xdotool mousemove_relative --sync 0 0
-    sleep "$delay"
+    usleep "$delay"
 done

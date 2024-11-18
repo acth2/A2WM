cc5dbb489f048e4f1e4e567a5a31054b3207bb71
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-11-18 21:23:18 +0100

    Create mousemover.cpp

---
+++ qt/windowmanager/mousemover.cpp
@@ -0,0 +1,25 @@
+#include "mousemover.h"
+
+void MouseMover::run() {
+    Display* display = XOpenDisplay(nullptr);
+    if (!display) {
+        qCritical() << "Unable to open X display";
+        return;
+    }
+    Window root = DefaultRootWindow(display);
+
+    while (true) {
+        Window returnedRoot, returnedChild;
+        int rootX, rootY, winX, winY;
+        unsigned int mask;
+
+        if (XQueryPointer(display, root, &returnedRoot, &returnedChild, 
+                          &rootX, &rootY, &winX, &winY, &mask)) {
+            XWarpPointer(display, None, root, 0, 0, 0, 0, rootX + 1, rootY);
+            XFlush(display);
+        }
+
+        usleep(1000);
+    }
+    XCloseDisplay(display);
+}

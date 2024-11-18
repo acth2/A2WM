60f323aee0a92e0bb4e8e86a64c4d71188642092
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-11-18 21:02:55 +0100

    Update main.cpp

--- qt/windowmanager/main.cpp
+++ qt/windowmanager/main.cpp
@@ -9,15 +9,48 @@
 #include <QMessageBox>
 #include <QGuiApplication>
 #include <QX11Info>
+#include <QThread>
 #include <cstdlib>
+#include <X11/Xlib.h>
 
 QFile logFile;
 
+// Custom logging function
 void customLogOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
     QTextStream out(&logFile);
     out << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz ")
         << msg << Qt::endl;
 }
+
+// Worker class for mouse movement
+class MouseMover : public QThread {
+    Q_OBJECT
+
+public:
+    void run() override {
+        Display* display = XOpenDisplay(nullptr);
+        if (!display) {
+            qCritical() << "Unable to open X display";
+            return;
+        }
+        Window root = DefaultRootWindow(display);
+
+        while (true) {
+            Window returnedRoot, returnedChild;
+            int rootX, rootY, winX, winY;
+            unsigned int mask;
+
+            if (XQueryPointer(display, root, &returnedRoot, &returnedChild, 
+                              &rootX, &rootY, &winX, &winY, &mask)) {
+                XWarpPointer(display, None, root, 0, 0, 0, 0, rootX + 1, rootY);
+                XFlush(display);
+            }
+
+            usleep(1000); // Sleep for 1 millisecond
+        }
+        XCloseDisplay(display);
+    }
+};
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -27,7 +60,7 @@ int main(int argc, char *argv[]) {
 
     qInstallMessageHandler(customLogOutput);
 
-    // Check if the window manager is booted with Xorg
+    // Check if the user use Wayland
     QString platform = QGuiApplication::platformName();
     if (!QX11Info::isPlatformX11()) {
         QMessageBox::critical(nullptr, "Error", "This window manager only works on X11/Xorg.\nPlease restart A2WM with X11\n\n[PRESS ENTER TO CONTINUE]");
@@ -37,7 +70,7 @@ int main(int argc, char *argv[]) {
     WindowManager manager;
     TaskBar taskBar;
 
-    // Create the wallpaper
+    // Create the window manager instance
     QScreen *screen = QApplication::primaryScreen();
     if (screen) {
         QRect screenGeometry = screen->geometry();
@@ -49,5 +82,11 @@ int main(int argc, char *argv[]) {
 
     // Create the taskbar
     taskBar.show();
+
+    // Start the mouse mover in a separated thread
+    MouseMover mouseMover;
+    mouseMover.start();
+    // /\ Help the window manager keep the actives windows enabled and not making them lag
+    
     return app.exec();
 }

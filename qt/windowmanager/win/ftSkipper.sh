c882cf0c531447fe22ea0f6c3f991f2ca1318dfa
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-11-18 17:23:28 +0100

    Update ftSkipper.sh

--- qt/windowmanager/win/ftSkipper.sh
+++ qt/windowmanager/win/ftSkipper.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
+# 60Hz means 0.0167 seconds
+refresh_rate=0.0167
+
 while true; do
-    idle_time=$(xprintidle)
-    if [ "$idle_time" -gt 500 ]; then  # Run only if idle > 500ms
-        xdotool mousemove_relative --sync 0 0
+    start=$(date +%s.%N)  # Record the start time
+    xdotool mousemove_relative --sync 0 0
+    end=$(date +%s.%N)  # Record the end time
+    duration=$(echo "$end - $start" | bc)
+    sleep_time=$(echo "$refresh_rate - $duration" | bc)
+    if (( $(echo "$sleep_time > 0" | bc -l) )); then
+        sleep "$sleep_time"
     fi
 done

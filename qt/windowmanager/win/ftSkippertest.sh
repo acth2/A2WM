d01d0395d6a44a325ec59332f3fdbf0e97a0afe2
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-11-19 21:07:39 +0100

    Update ftSkippertest.sh

--- qt/windowmanager/win/ftSkippertest.sh
+++ qt/windowmanager/win/ftSkippertest.sh
@@ -1,32 +1,21 @@
-import threading
-import time
-import pynput.keyboard
-from pynput.keyboard import Controller
+from pynput.keyboard import Controller, Key
 
-# Initialisation du contrôleur clavier
 keyboard = Controller()
 
 
-def hold_key(key):
-    """Appuie sur une touche de manière prolongée."""
-    while True:
-        keyboard.press(key)  # Maintient la touche enfoncée
-        time.sleep(0.1)
-
-
 def main():
-    # Choisir une touche à maintenir (par exemple 'a')
-    key_to_hold = pynput.keyboard.Key.pause
-    print(f"La touche '{key_to_hold}' est maintenant maintenue enfoncée.")
+    key_to_hold = Key.pause
+
+    keyboard.press(key_to_hold)
+    print(f"La touche '{key_to_hold}' est maintenant maintenue enfoncee.")
     print("Appuyez sur Ctrl+C pour arrêter le script.")
 
     try:
-        # Garde le script actif sans consommer trop de ressources
         while True:
-            keyboard.press(key_to_hold)  # Maintient la touche enfoncée
-            time.sleep(0.1)
+            pass
     except KeyboardInterrupt:
-        print("\nArrêt du script.")
+        keyboard.release(key_to_hold)
+        print("\nTouche relachee. Arrêt du script.")
 
 
 if __name__ == "__main__":

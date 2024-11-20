8330d3f770c477fdfb870d4553c095fd04b8e13a
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-11-20 11:14:42 +0100

    Add files via upload

---
+++ ftSkipper/out/production/ftSkipper/fr/acth2/ftSkipper/README.md
@@ -0,0 +1,2 @@
+<h1>The frame trotter disabler</h1>
+<p>made in Java by AcTh2</p>
---
+++ ftSkipper/src/fr/acth2/ftSkipper/Main.java
@@ -0,0 +1,27 @@
+package fr.acth2.ftSkipper;
+import oracle.jrockit.jfr.JFR;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static fr.acth2.ftSkipper.logger.Logger.*;
+
+public class Main extends Thread {
+    public static void main(String[] args) {
+        log("FtSkipper: Initialization");
+        Main mt = new Main();
+        mt.start();
+    }
+
+    @Override
+    public void run() {
+        Robot bot = null;
+        try { bot = new Robot();} catch (AWTException e) { throw new RuntimeException(e); }
+        while (true) {
+            PointerInfo a = MouseInfo.getPointerInfo();
+            Point b = a.getLocation();
+            bot.mouseMove((int) b.getX(), (int) b.getY());
+            System.out.println("OHH JE MARCHE");
+        }
+    }
+}

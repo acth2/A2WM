66335e0c0c8d2251394fb700d486432b34c25c5a
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-12-04 11:54:18 +0100

    Update taskbar.cpp

--- qt/windowmanager/taskbar.cpp
+++ qt/windowmanager/taskbar.cpp
@@ -342,7 +342,7 @@ void TaskBar::showPopup() {
         popup->move(0, screenGeometry.height() - popup->height() - 40);
         
         popupCenter->move(37, popup->y() + 75);
-        popupExtension->move(popup->width() - popupExtension->width() / 2, popup->height() + popup->height() / 6);
+        popupExtension->move(popup->width() - popupExtension->width() / 2, popup->height() + popup->height() / 8);
 
         popup->setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);
         popupExtension->setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);

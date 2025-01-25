2da49afbd37a9027cc4dfd244f6fbb432a423357
acth2 <iliane57950@gmail.com>
2025-01-25 23:10:25 +0100

    fixing the os checker functions

--- src/fr/acth2/a2wm/Main.java
+++ src/fr/acth2/a2wm/Main.java
@@ -9,7 +9,7 @@ import static fr.acth2.a2wm.utils.logger.Logger.*;
 
 public class Main {
     public static void main(String[] args) {
-        if(!isUnix()) {
+        if(!isLinux()) {
             err("The window-manager is not compatible with non-unix os.");
             System.exit(0);
         }

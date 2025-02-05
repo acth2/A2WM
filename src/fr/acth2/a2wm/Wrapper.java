787f946d26e4664712672ae6dfdd7f73541fe6e5
acth2 <iliane57950@gmail.com>
2025-02-05 17:42:42 +0100

    Adding an args logic

--- src/fr/acth2/a2wm/Wrapper.java
+++ src/fr/acth2/a2wm/Wrapper.java
@@ -1,16 +1,31 @@
 package fr.acth2.a2wm;
 
 import com.formdev.flatlaf.FlatDarkLaf;
-import com.sun.org.apache.regexp.internal.RE;
 import fr.acth2.a2wm.components.background.BackgroundWindow;
 import fr.acth2.a2wm.components.taskbar.TaskbarWindow;
 import javax.swing.*;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static fr.acth2.a2wm.utils.References.*;
 import static fr.acth2.a2wm.utils.logger.Logger.*;
 
 public class Wrapper {
     public static void main(String[] args) {
+        AtomicBoolean startgui = new AtomicBoolean(true);
+        for (String arg : args) {
+            if ("--help".equalsIgnoreCase(arg)) {
+                log(YELLOW + "A2WM Help page ---- [1]" + RESET);
+                log("--no-gui: Does not start the gui");
+                System.exit(0);
+            }
+
+            if ("--no-gui".equalsIgnoreCase(arg)) {
+                log(YELLOW + "--no-gui argument found" + RESET);
+                startgui.set(false);
+            }
+        }
+
         if(!isLinux()) {
             err("The window-manager is not compatible with non-unix os.");
             System.exit(0);
@@ -19,11 +34,11 @@ public class Wrapper {
 
         String displayEnv = System.getenv("DISPLAY");
         if (displayEnv != null && !displayEnv.isEmpty()) {
-            startWM();
+            startWM(startgui.get());
         } else {
             String sessionType = System.getenv("XDG_SESSION_TYPE");
             if ("x11".equalsIgnoreCase(sessionType)) {
-                startWM();
+                startWM(startgui.get());
             } else if ("wayland".equalsIgnoreCase(sessionType)) {
                 log("A2WM is not likely to work on wayland.\nPlease use X11, however you can still trick the wm and start it anyway by defining the variable DISPLAY");
                 System.exit(0);
@@ -44,26 +59,28 @@ public class Wrapper {
         }
     }
 
-    private static void startWM() {
-        if (isAppAvailable("wmctrl")) {
-            log(YELLOW + "STARTING " + RESET + NAME);
-            log(YELLOW + "VERSION " + RESET + VERSION);
+    private static void startWM(boolean startgui) {
+        if (startgui) {
+            if (isAppAvailable("wmctrl")) {
+                log(YELLOW + "STARTING " + RESET + NAME);
+                log(YELLOW + "VERSION " + RESET + VERSION);
 
-            try {
-                UIManager.setLookAndFeel(new FlatDarkLaf());
-                SwingUtilities.invokeLater(() -> {
-                    BackgroundWindow backgroundWindow = new BackgroundWindow();
-                });
+                try {
+                    UIManager.setLookAndFeel(new FlatDarkLaf());
+                    SwingUtilities.invokeLater(() -> {
+                        BackgroundWindow backgroundWindow = new BackgroundWindow();
+                    });
 
-                SwingUtilities.invokeLater(() -> {
-                    TaskbarWindow taskbarWindow = new TaskbarWindow();
-                });
-            } catch (Exception exception) {
-                err(NAME + " CRASHED!\n");
-                exception.printStackTrace();
+                    SwingUtilities.invokeLater(() -> {
+                        TaskbarWindow taskbarWindow = new TaskbarWindow();
+                    });
+                } catch (Exception exception) {
+                    err(NAME + " CRASHED!\n");
+                    exception.printStackTrace();
+                }
+            } else {
+                err("The application 'wmctrl' is not found in your PATH.\nPlease install this software and restart the windowmanager");
             }
-        } else {
-            err("The application 'wmctrl' is not found in your PATH.\nPlease install this software and restart the windowmanager");
         }
     }
 }
